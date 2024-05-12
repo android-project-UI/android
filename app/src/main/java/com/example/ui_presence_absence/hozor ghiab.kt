d155ee7ee7982d47451112fa7273910e3660a685
Pouria Sameti <pouria.sameti80@gmail.com>
2024-05-12 20:07:30 +0330

    hozor ghiab page -> student information part first implementation.

--- app/src/main/java/com/example/ui_presence_absence/hozor ghiab.kt
+++ app/src/main/java/com/example/ui_presence_absence/hozor ghiab.kt
@@ -8,9 +8,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -42,8 +47,14 @@ fun hozorGhiab(){
 
 
     val sessionName = "ساختمان های داده"
-    val sessionNumber = 7
+    val sessionNumber = 13
     val sessionDate = "تاریخ: " + "1402/9/2"
+
+    val studentMap = mapOf("993623030" to "علیرضا کریمی",
+        "993623031" to "محمد همدانی",
+        "993623032" to "کیانا چکناواریان",
+        "993623035" to "علی همدانی",
+        "993623037" to "علی همدانی")
 
 
 
@@ -86,59 +97,157 @@ fun hozorGhiab(){
             .padding(10.dp))
         {
 
-            Row(modifier = Modifier
-                .width(screenWidth.dp)
-                .height(100.dp)
-                .border (1.dp,Color.Black, shape = RoundedCornerShape(15.dp))
-                .background(color = Color(0xFFD9D9D9), shape = RoundedCornerShape(15.dp))
-                ,
+            // class information
+            Row(
+                modifier = Modifier
+                    .width(screenWidth.dp)
+                    .height(100.dp)
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(15.dp))
+                    .background(color = Color(0xFFD9D9D9), shape = RoundedCornerShape(15.dp)),
                 horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically){
+                verticalAlignment = Alignment.CenterVertically
+            ) {
 
-                Column(modifier = Modifier.width(140.dp)
-                    .padding(10.dp)) {
+                Column(
+                    modifier = Modifier
+                        .width(140.dp)
+                        .padding(10.dp)
+                ) {
 
-                    Column(modifier = Modifier.width(70.dp)
-                                            .height(70.dp)
-                                            .background(color = Color(0xFFF5F5F5),
-                                                        shape = RoundedCornerShape(15.dp)),
+                    Column(
+                        modifier = Modifier
+                            .width(70.dp)
+                            .height(70.dp)
+                            .background(
+                                color = Color(0xFFF5F5F5),
+                                shape = RoundedCornerShape(15.dp)
+                            ),
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.Center
-                    ){
+                    ) {
 
-                        Text(text = sessionNumber.toString(),
-                            style = TextStyle(fontSize = 20.sp,
-                                            fontFamily = FontFamily(font),
-                                            fontWeight = FontWeight(400),
-                                            color = Color(0xFF000000),
-                                            textAlign = TextAlign.Center,
-                            ))
-                        Text(text = "جلسه",
-                            style = TextStyle(fontSize = 15.sp,
+                        Text(
+                            text = sessionNumber.toString(),
+                            style = TextStyle(
+                                fontSize = 20.sp,
                                 fontFamily = FontFamily(font),
                                 fontWeight = FontWeight(400),
                                 color = Color(0xFF000000),
                                 textAlign = TextAlign.Center,
-                            ))
+                            )
+                        )
+                        Text(
+                            text = "جلسه",
+                            style = TextStyle(
+                                fontSize = 15.sp,
+                                fontFamily = FontFamily(font),
+                                fontWeight = FontWeight(400),
+                                color = Color(0xFF000000),
+                                textAlign = TextAlign.Center,
+                            )
+                        )
                     }
                 }
 
-                Column(modifier = Modifier
-                    .width(250.dp)
-                    .padding(10.dp),
-                    horizontalAlignment = Alignment.End) {
+                Column(
+                    modifier = Modifier
+                        .width(250.dp)
+                        .padding(10.dp),
+                    horizontalAlignment = Alignment.End
+                ) {
 
-                    Text(text = sessionName, style = TextStyle(fontSize = 25.sp,
-                                                            fontFamily = FontFamily(font),
-                                                            fontWeight = FontWeight(400),
-                                                            color = Color(0xFF000000),
-                                                            textAlign = TextAlign.Right))
+                    Text(
+                        text = sessionName, style = TextStyle(
+                            fontSize = 25.sp,
+                            fontFamily = FontFamily(font),
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Right
+                        )
+                    )
 
-                    Text(text = sessionDate, style = TextStyle(fontSize = 18.sp,
-                                                                fontFamily = FontFamily(font),
-                                                                fontWeight = FontWeight(400),
-                                                                color = Color(0xFF000000),
-                                                                textAlign = TextAlign.Right))
+                    Text(
+                        text = sessionDate, style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = FontFamily(font),
+                            fontWeight = FontWeight(400),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Right
+                        )
+                    )
+                }
+            }
+
+            // student information
+            Column(
+                modifier = Modifier
+                    .width(screenWidth.dp)
+                    .height(400.dp)
+                    .padding(vertical = 10.dp, horizontal = 1.dp)
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(15.dp))
+                    .background(
+                        color = MaterialTheme.colorScheme.background,
+                        shape = RoundedCornerShape(15.dp)
+                    )
+                    .verticalScroll(rememberScrollState()),
+            ) {
+
+                for (key in studentMap.keys) {
+                    Row(
+                        modifier = Modifier
+                            .width(390.dp)
+                            .height(80.dp)
+                            .padding(5.dp)
+                            .background(
+                                color = MaterialTheme.colorScheme.onBackground,
+                                shape = RoundedCornerShape(10.dp)
+                            ),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.End
+                    ) {
+
+                        Column(
+                            modifier = Modifier
+                                .width(150.dp)
+                                .padding(5.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Text(
+                                text = "${studentMap[key]}",
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                    fontFamily = FontFamily(font),
+                                    fontWeight = FontWeight(400),
+                                    color = Color(0xFF000000),
+                                    textAlign = TextAlign.Right
+                                )
+                            )
+
+                            Text(
+                                text = "$key",
+                                style = TextStyle(
+                                    fontSize = 15.sp,
+                                    fontFamily = FontFamily(font),
+                                    fontWeight = FontWeight(400),
+                                    color = Color(0xFF000000),
+                                    textAlign = TextAlign.Right
+                                )
+                            )
+                        }
+
+                        Column(
+                            modifier = Modifier
+                                .width(60.dp)
+                                .padding(5.dp)
+                        ) {
+                            val studentImage = painterResource(id = R.drawable.user)
+                            Image(
+                                painter = studentImage, contentDescription = "",
+                                modifier = Modifier.size(56.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
