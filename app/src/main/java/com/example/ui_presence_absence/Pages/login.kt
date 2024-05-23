8135ecad32b21c45a64e672e905a254364fc0c6f
Pouria Sameti <pouria.sameti80@gmail.com>
2024-05-23 23:01:25 +0330

    Login page -> logic of page implementation.

--- app/src/main/java/com/example/ui_presence_absence/Pages/login.kt
+++ app/src/main/java/com/example/ui_presence_absence/Pages/login.kt
@@ -1,4 +1,5 @@
 package com.example.ui_presence_absence.Pages
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -6,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,18 +16,27 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,6 +45,7 @@ import androidx.navigation.NavController
 import com.example.ui_presence_absence.Destination
 import com.example.ui_presence_absence.MainActivity
 import com.example.ui_presence_absence.R
+import com.example.ui_presence_absence.model.getMaster
 
 @Preview
 @Composable
@@ -42,9 +54,13 @@ fun Login() {
     val screenWidth = 420
     val screenHeight = 740
     val bodyHeight = 680
-    val CIRCULAR_TIMER_RADIUS = 1
     val font = Font(R.font.koodak)
     val pageSubject = "ورود"
+
+    var username by remember { mutableStateOf("")}
+    var password by remember { mutableStateOf("")}
+
+    var masterId by remember { mutableStateOf("")}
 
 
     Column(
@@ -90,9 +106,79 @@ fun Login() {
             modifier = Modifier
                 .width(screenWidth.dp)
                 .height(bodyHeight.dp)
-                .padding(10.dp)
+                .padding(20.dp)
         )
-        {}
+        {
+
+            Box() {
+                OutlinedTextField(
+                    value = username,
+                    onValueChange = { username = it },
+                    label = { Text("نام کاربری", style = TextStyle(
+                                                            fontSize = 20.sp,
+                                                            fontFamily = FontFamily(font),
+                                                            fontWeight = FontWeight(200),
+                                                            color = MaterialTheme.colorScheme.primary,
+                                                            textAlign = TextAlign.Right
+                                                        )) },
+
+                    modifier = Modifier
+                        .width(screenWidth.dp)
+                        .height(70.dp),
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Box() {
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("رمز عبور", style = TextStyle(
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily(font),
+                        fontWeight = FontWeight(200),
+                        color = MaterialTheme.colorScheme.primary,
+                        textAlign = TextAlign.Right
+                    )) },
+
+                    modifier = Modifier
+                        .width(screenWidth.dp)
+                        .height(70.dp),
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Button(
+                onClick = {
+                    val master  = getMaster(username.toString())
+                    if (master != null) {
+                        masterId = master.id
+                    }
+                },
+                modifier = Modifier
+                    .width(screenWidth.dp)
+                    .height(60.dp),
+                shape = RoundedCornerShape(15.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = MaterialTheme.colorScheme.onSecondary
+                )
+            ) {
+                Text(
+                    text = "اعمال", style = TextStyle(
+                        fontSize = 28.sp,
+                        fontFamily = FontFamily(font),
+                        fontWeight = FontWeight(400),
+                        color = MaterialTheme.colorScheme.onSecondary,
+                        textAlign = TextAlign.Center
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+        }
 
 
         // Footer initialization
