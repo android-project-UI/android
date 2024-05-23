4fae8c69eaf3997b0e80c72b9dea62090a7c370b
Pouria Sameti <pouria.sameti80@gmail.com>
2024-05-23 18:51:44 +0330

    Session.kt -> class defenition was changed.

--- app/src/main/java/com/example/ui_presence_absence/model/Session.kt
+++ app/src/main/java/com/example/ui_presence_absence/model/Session.kt
@@ -1,10 +1,11 @@
 package com.example.ui_presence_absence.model
 
-data class Session(
-    var id: String,
-    var title: String,
-    var description: String,
-    var date: String,
-    var owner: Master,
-    var presentStudents:List<Student>
-)
+
+val allSessions = mutableMapOf<String, Session>()
+class Session(var id: String, var title: String, var description: String, var date: String,
+              var owner: Master,var presentStudents:List<Student>){
+
+    init {
+        allSessions.put(id, this)
+    }
+}
