335513be82e697ef3496e2f6bd73d5c29e1a7e15
Pouria Sameti <pouria.sameti80@gmail.com>
2024-05-23 19:33:01 +0330

    Master.kt -> createMaster() were implemented.

--- app/src/main/java/com/example/ui_presence_absence/model/Master.kt
+++ app/src/main/java/com/example/ui_presence_absence/model/Master.kt
@@ -4,7 +4,7 @@ package com.example.ui_presence_absence.model
 
 private var allMasters = mutableMapOf<String, Master>()
 class Master(var id: String, var name: String, var image: String, var username: String,
-             var password: String)
+             var password: String, var group: String)
 {
     init {
         allMasters.put(id, this)
@@ -13,4 +13,15 @@ class Master(var id: String, var name: String, var image: String, var username: 
 
 fun getMaster(id: String): Master? {
     return allMasters.get(id)
+}
+
+fun createMaster(){
+    val m1 = Master("9910", "محمد برزوئی", "user", "9910", "9910",
+        group = "شبکه")
+
+    val m2 = Master("9911", "پروین شیخ الاسلام", "user", "9911", "9911",
+        group = "هوش مصنوعی")
+
+    val m3 = Master("9912", "مریم جمالی", "user", "9912", "9912",
+        group = "نرم افزار")
 }
