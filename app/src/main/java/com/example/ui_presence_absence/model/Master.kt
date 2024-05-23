6b4be0002da9274fa71d5725bc8ceca4fc457b3b
Pouria Sameti <pouria.sameti80@gmail.com>
2024-05-23 18:48:48 +0330

    Master.kt -> getMaster() was declared.

--- app/src/main/java/com/example/ui_presence_absence/model/Master.kt
+++ app/src/main/java/com/example/ui_presence_absence/model/Master.kt
@@ -6,8 +6,11 @@ private var allMasters = mutableMapOf<String, Master>()
 class Master(var id: String, var name: String, var image: String, var username: String,
              var password: String, var sessions: List<Session>)
 {
-
     init {
         allMasters.put(id, this)
     }
+}
+
+fun getMaster(id: String): Master? {
+    return allMasters.get(id)
 }
