bcb8e175a0a9f0805233bc8db9559040831716df
Pouria Sameti <pouria.sameti80@gmail.com>
2024-05-23 18:44:39 +0330

    Master.kt -> Master class defenition was changed.

--- app/src/main/java/com/example/ui_presence_absence/model/Master.kt
+++ app/src/main/java/com/example/ui_presence_absence/model/Master.kt
@@ -1,13 +1,13 @@
 package com.example.ui_presence_absence.model
 
 
-import android.media.Image
 
-data class Master(
-    var id: String,
-    var name: String,
-    var image: Image,
-    var username: String,
-    var password: String,
-    var sessions: List<Session>
-)
+private var allMasters = mutableMapOf<String, Master>()
+class Master(var id: String, var name: String, var image: String, var username: String,
+             var password: String, var sessions: List<Session>)
+{
+
+    init {
+        allMasters.put(id, this)
+    }
+}
