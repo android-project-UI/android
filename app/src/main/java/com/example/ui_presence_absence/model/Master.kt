8cfbcd3432ca64293806b2710e45dab87b775478
Pouria Sameti <pouria.sameti80@gmail.com>
2024-05-23 17:23:43 +0330

    review in models.

--- app/src/main/java/com/example/ui_presence_absence/model/Master.kt
+++ app/src/main/java/com/example/ui_presence_absence/model/Master.kt
@@ -4,9 +4,8 @@ package com.example.ui_presence_absence.model
 import android.media.Image
 
 data class Master(
-    var id: Int,
-    var name: Int,
-    var family: String,
+    var id: String,
+    var name: String,
     var image: Image,
     var username: String,
     var password: String,
--- app/src/main/java/com/example/ui_presence_absence/model/Session.kt
+++ app/src/main/java/com/example/ui_presence_absence/model/Session.kt
@@ -1,7 +1,7 @@
 package com.example.ui_presence_absence.model
 
 data class Session(
-    var id: Int,
+    var id: String,
     var title: String,
     var description: String,
     var date: String,
